1c5f65636c019d6d5ade489ceff8dc2f1fbbd114
Helle Juul Hansen <hellejuulpedersen@gmail.com>
2021-12-08 10:56:19 +0100

    Added a page for comparing trends in number of Sales

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -48,6 +49,209 @@ app.layout = dbc.Container(
     ],
     fluid=True,
 )
+
+### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ###
+
+###         P A G E   0  -  Number of sales                                                         ###
+
+### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ###
+
+#
+# Loading the data
+#
+
+# Geojson file with the shape of the zip code areas on Fyn with a unique ID pr shape 
+with open('zip_code_areas_fyn_with_id.geojson', "r") as f:
+    zip_code_areas = json.load(f)
+
+# Dataset with the number of sales per quarter optimized for the map
+with open('total_sales_by_quarter.csv', "r") as f:
+    total_sales = pd.read_csv(f)
+
+# Dataset with the number of sales per quarter optimized for the bar chart
+with open('total_sales_by_quarter_for_bar.csv', "r") as f:
+    total_sales_bar = pd.read_csv(f)
+
+
+#
+# Creating the content
+#
+
+# A list with the quarters covered in the dataset
+quarters = list(total_sales.quarter_name.unique())
+# Slider for choosing the month and year
+marks = {i: {'label': label} for i, label in enumerate(quarters)}
+
+total_sales_slider = dcc.Slider(id='month_slider_total_sales',
+                                min=0, 
+                                max=len(quarters)-1, 
+                                value=len(quarters)-1,
+                                step=1,
+                                marks=marks,
+                                included=False)
+
+# Dropdown menu for selecting zip code areas
+zips_and_names = total_sales[['zip_code', 'name']].drop_duplicates()
+zips_and_names.sort_values('zip_code', inplace=True)
+zipped = zip(zips_and_names.zip_code, zips_and_names.name)
+dropdown_options_total_sales = [{'label': str(z) + " " + str(name), 'value': z} for z, name in zipped]
+
+total_sales_dropdown = dcc.Dropdown(id="zip_dropdown_total_sales",
+                                options=dropdown_options_total_sales,
+                                value=[5000, 5900],
+                                multi=True)
+
+
+# The content of the page styled to be two rows
+page0 = [
+        dbc.Container(
+            [   html.H2("Number of Houses Sold"),
+                dbc.RadioItems(options = [{'label': 'Number of Sales', 'value': 'abs_num'},
+                                          {'label': 'Sales per 1000 Residences', 'value': 'rel_num'}],
+                               value = 'abs_num',
+                               labelStyle = {'display': 'inline-block'},
+                               id = "rel_abs_radio"),
+                html.Br(),   
+                html.H4(id="total_sales_header", children="2021 - Q4"),      
+                dcc.Graph(id='total_sales_map'),
+                html.Div("Choose Quarter", className="form-label"),
+                total_sales_slider
+            ],
+            style={'width':'50%', 'display':'inline-block', 'vertical-align':'top'}
+        ), 
+        dbc.Container(
+            [   
+                html.H4("Development over Time"),
+                html.Div("Choose Zip Code Areas", className="form-label"),
+                total_sales_dropdown,
+                dcc.Graph(id="total_sales_bar")
+            ], 
+            style={'width':'50%','display':'inline-block', 'vertical-align':'top'}
+        ),
+    ]
+
+
+#
+#   Callbacks
+#
+
+@app.callback(
+    Output("total_sales_map", "figure"),
+    Input("rel_abs_radio", "value"),
+    Input("month_slider_total_sales", "value"),
+    Input("zip_dropdown_total_sales", "value")
+    )
+def update_choropleth_with_total_sales(rel_or_abs, date, selected_zips):
+    '''Create and update the choropleth map of Fyn with number of sold houses'''
+    if rel_or_abs == "rel_num":
+        color = "rel_sales"
+        range_color = [0, 75]
+        color_scale = "dense"
+    else:
+        color = "sales"
+        range_color = [0, 150]
+        color_scale = "dense"
+    selected_date = quarters[date]
+    total_sales_selected_month = total_sales[total_sales.quarter_name==selected_date]
+    # Change the coloring according to the chosen month
+    fig = px.choropleth(total_sales_selected_month,
+                geojson=zip_code_areas, 
+                color=color, 
+                locations='id',
+                projection="mercator",
+                range_color=range_color,
+                color_continuous_scale=color_scale,
+                template='simple_white',
+                labels={"sales": 'Number of Sales',
+                        "rel_sales": "Sales per 1000 Residences"})
+    # Zoom in on Fyn
+    fig.update_geos(fitbounds="locations", visible=False)
+    # Remove margins
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    # Move colorbar to the left
+    fig.update_layout(coloraxis_colorbar_x=-0.08)
+    # Make the hover data look nice
+    area_zips_and_names = list(total_sales['zip_code'].apply(str) + " " + total_sales['name'].apply(str))
+    fig.update_traces(hovertemplate='<b>%{customdata}</b><br>%{z}', 
+                      customdata=area_zips_and_names)
+    # Highlight selected zips on the map
+    selected_areas_ids = translate_zips_to_ids(selected_zips)
+    fig.add_trace(go.Choropleth(geojson=zip_code_areas,
+                                locationmode="geojson-id",
+                                locations=selected_areas_ids,
+                                z = [1]*len(selected_areas_ids),
+                                colorscale = [[0, 'rgba(0,0,0,0)'],[1, 'rgba(0,0,0,0)']],
+                                colorbar=None,
+                                showscale =False,
+                                marker = {"line": {"color": "#F39C12", "width": 1}},
+                                hoverinfo='skip'))
+    return  fig
+
+
+@app.callback(
+    Output("total_sales_header", "children"),
+    Input("month_slider_total_sales", "value")
+    )
+def update_title_to_match_chosen_quarter_total_sales(date):
+    '''Change the title to show the chosen month and year.'''
+    return  quarters[date]
+
+
+@app.callback(
+    Output("zip_dropdown_total_sales", "value"),
+    Input("total_sales_map", "clickData"),
+    State("zip_dropdown_total_sales", "value")   
+)
+def update_dropdown_total_sales(clickData, selected_zips):
+    '''Add regions selected on the choropleth map to the selected values in 
+    the dropdown menu.'''
+    if clickData:
+        zip_from_map = int(clickData['points'][0]['customdata'][0:4])
+        if zip_from_map not in selected_zips:
+            selected_zips.append(zip_from_map)
+    return selected_zips
+
+
+@app.callback(
+    Output("total_sales_bar", "figure"),
+    Input("rel_abs_radio", "value"),
+    Input("zip_dropdown_total_sales", "value"),
+    Input("month_slider_total_sales", "value"),
+    )
+def update_line_chart_with_total_sales(rel_or_abs, selected_zips, date):
+    '''Create and update the line chart showing the development in m2 prices
+    in the selected zip code areas'''
+    if len(selected_zips) == 0:
+        return {}
+    if rel_or_abs == "rel_num":
+        y_value = "rel_sales"
+    else:
+        y_value = "sales"
+    total_sales_filtered = total_sales_bar[total_sales_bar.zip_code.isin(selected_zips)]
+    # Draw a line for each of the selected zip code areas
+    fig = px.bar(total_sales_filtered, 
+                  x='quarter_name', 
+                  y=y_value,
+                  color=total_sales_filtered.zip_code.apply(str),
+                  template='simple_white',
+                  color_discrete_sequence=px.colors.qualitative.Vivid,
+                  facet_row='zip_code',
+                  height=600,
+                  labels={'zip_code':   'Zip Code',
+                          'color':      'Zip code',
+                          'quarter_name':    'Quarter',
+                          'sales':  'Number of Sales',
+                          'rel_sales': "Sales per 1000<br>Residences"})
+    # Smaller text on y-axis
+    fig.update_yaxes(title_font=dict(size=10))
+    # Add a rectangle to highlight the selected quarter
+    selected_date = quarters[date]
+    fig.add_vrect(x0=selected_date, x1=selected_date, col=1,
+              fillcolor="green", opacity=0.10, line_width=45)
+    # Make hover data look nice
+    fig.update_traces(hovertemplate=None)
+    fig.update_layout(hovermode="x")
+    return fig
 
 
 ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ### -o-o- ###
@@ -335,7 +539,7 @@ page3 = html.P("You found page 3!")
     )
 def render_page_content(pathname):
     if pathname == "/":
-        return html.P("This is the content of the home page!")
+        return page0
     elif pathname == "/page-1":
         return page1
     elif pathname == "/page-2":
