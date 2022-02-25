0136ac0a41740982088f39f0b3ef3f918ea08e6e
Helle Juul Hansen <hellejuulpedersen@gmail.com>
2022-02-25 13:36:09 +0100

    Cleaning Up

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from dash import Input, Output, dcc, html
 import dash_bootstrap_components as dbc
 
 from app import app
-from pages import salesprices, m2prices, totalsales, overview, salesprices_and_overview
+from pages import m2prices, totalsales, salesprices_and_overview
 
 # Setting the main layout with a fixed navbar at the top
 navbar = dbc.Navbar(
@@ -19,7 +19,6 @@ navbar = dbc.Navbar(
             dbc.NavItem(dbc.NavLink("Search in House Sales", href="/", active='exact')),
             dbc.NavItem(dbc.NavLink("Average Price per m2", href="/page-1", active='exact')),
             dbc.NavItem(dbc.NavLink("Number of Houses Sold", href="/page-2", active='exact')),
-            #dbc.NavItem(dbc.NavLink("Overview of Houses Sold", href="/page-3", active='exact')),
         ]
     ),
     color="primary",
@@ -43,17 +42,15 @@ app.layout = dbc.Container(
     )
 def render_page_content(pathname):
     if pathname == "/":
-        return salesprices_and_overview.page    #salesprices.page2
+        return salesprices_and_overview.page
     elif pathname == "/page-1":
         return m2prices.page1
     elif pathname == "/page-2":
         return totalsales.page0
-    #elif pathname == "/page-3":
-    #    return overview.page3
     # If the user tries to reach a different page, return a 404 message
     else:
         return html.H1("404: Not found", className="text-danger")
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
